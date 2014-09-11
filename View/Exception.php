be59b129ac1aa41ca3a052ade0f593e81ac9e505
Pete Smith <pete@peteonline.co.uk>
2014-09-11 11:39:40 +0100

    Includes update to View\Exception for previous commit

--- View/Exception.php
+++ View/Exception.php
@@ -9,6 +9,9 @@ extends \PO\Exception
 	const TEMPLATE_PATH_NOT_STRING					= 0;
 	const TEMPLATE_FILE_COULD_NOT_BE_IDENTIFIED		= 1;
 	const TEMPLATE_VARIABLES_NOT_ASSOCIATIVE_ARRAY	= 2;
+	const ANCESTOR_CLASS_DOES_NOT_EXIST				= 3;
+	const ANCESTOR_CLASS_NOT_ANCESTOR				= 4;
+	const RENDER_INTO_METHOD_DOES_NOT_EXIST			= 5;
 	
 	protected function getMessageFromCode($code)
 	{
@@ -26,6 +29,19 @@ extends \PO\Exception
 			case Exception::TEMPLATE_VARIABLES_NOT_ASSOCIATIVE_ARRAY:
 				return 'The template variables provided to the constructor ' .
 					'were not in a key/value format';
+			break;
+			
+			case Exception::ANCESTOR_CLASS_DOES_NOT_EXIST:
+				return 'The class provided as an example of an ancestor does not exist';
+			break;
+			
+			case Exception::ANCESTOR_CLASS_NOT_ANCESTOR:
+				return 'The provided class is not an ancestor of this class';
+			break;
+			
+			case Exception::RENDER_INTO_METHOD_DOES_NOT_EXIST:
+				return 'The object provided to render the current view into ' .
+					'does not have the provided method';
 			break;
 			
 		}
